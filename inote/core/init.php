e4d926c67950d4baa7449d55da87ab5aae6d4e6f
nqanh0110 <nguyenandie1905@gmail.com>
2021-04-19 15:24:34 +0700

    add

--- inote/core/init.php
+++ inote/core/init.php
@@ -1,3 +1,35 @@
 <!-- file trung tâm của ứng dụng, có thể nói nó là file trung gian để include các class, 
 kết nối database, lưu session và xử lý các thông tin chung 
 -->
+<?php
+
+// Include các thư viện
+require_once 'classes/DB.php';
+require_once 'classes/Session.php';
+
+// Khởi tạo object DB
+$db = new DB();
+// Kết nối database
+$db->connect();
+
+// Khởi tạo object Session
+$session = new Session();
+// Bắt đầu session
+$session->start();
+// Lấy dữ liệu session
+$user = $session->get();
+// Múi giờ chung
+date_default_timezone_set('Asia/Ho_Chi_Minh');
+$date_current = '';
+$date_current = date("Y-m-d H:i:sa");
+
+// Nếu tồn tại $user
+if ($user) {
+    // Lệnh truy vấn thông tin user
+    $sql_get_data_user = "SELECT * FROM users WHERE username = '$user'";
+
+    // Lấy thông tin user
+    $data_user = $db->fetch_assoc($sql_get_data_user, 1);
+}
+
+?>
--- inote/signout.php
+++ inote/signout.php
@@ -1,1 +1,11 @@
 <!-- file giải phóng session, nó dùng để đăng xuất -->
+<?php
+
+// Include database, session, general info
+require_once 'core/init.php';
+// Xoá session
+$session->destroy();
+// Trở về trang chủ
+header('Location: index.php');
+
+?>
